34cf227c65842d7448cb87e66ea2981c98fcbd7c
Robi <rahail79@iMac714-462.local>
2019-11-15 14:30:42 -0500

    Project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/download.jpeg" />
+    <h1>hello</h1>
+    <button type="button">Apple</button>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
 </script>
