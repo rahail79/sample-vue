619347a55415cf257c46cfcb839a209ce671b9e7
Robi <rahail79@iMac714-462.local>
2019-11-20 14:34:27 -0500

    project

---
+++ src/views/About2.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="About2">
+    <h1>This is an about page</h1>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="home">
-<<<<<<< HEAD
+
     <img alt="Vue logo" src="../assets/download.jpeg" />
     <h1>hello</h1>
     <button type="button">Apple</button>
-=======
+
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <b-button>Hello</b-button>
->>>>>>> fcb60adc8555ce6d237a00fc70006b19ec9cd1d9
   </div>
 </template>
 <script>
