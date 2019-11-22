05503469e9f96bd92ad7116e49f00881f312e8f4
Robi <rahail79@iMac714-462.local>
2019-11-22 14:33:42 -0500

    Project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,12 @@
   <div class="home">
 
     <img alt="Vue logo" src="../assets/download.jpeg" />
-    <h1>hello</h1>
+    <h1>History of smartphones</h1>
     <button type="button">Apple</button>
 
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <b-button>Hello</b-button>
+
   </div>
 </template>
 <script>
