4da7f29b4d3af12f9116586db33a639304028b18
Robi <rahail79@iMac714-462.local>
2019-11-26 14:27:43 -0500

    project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,22 @@
 
     <img alt="Vue logo" src="../assets/download.jpeg" />
     <h1>History of smartphones</h1>
-    <button type="button">Apple</button>
-
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <b-button>Hello</b-button>
+
+
+    <!-- Collapse -->
+    <div>
+  <b-button v-b-toggle.collapse-1 variant="primary">History</b-button>
+  <b-collapse id="collapse-1" class="mt-2">
+    <b-card>
+      <p class="card-text">Collapse contents Here</p>
+      <b-button v-b-toggle.collapse-1-inner size="sm">Toggle Inner Collapse</b-button>
+      <b-collapse id="collapse-1-inner" class="mt-2">
+        <b-card>Hello!</b-card>
+      </b-collapse>
+    </b-card>
+  </b-collapse>
+</div>
 
   </div>
 </template>
