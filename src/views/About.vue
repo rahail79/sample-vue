778c29ed2146a1e37f67c9e72291daeba69f68ce
Robi <rahail79@iMac714-462.local>
2019-12-03 14:35:25 -0500

    project

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,16 +2,22 @@
   <div class="about">
 <img alt="Vue logo" src="../assets/logo.png" />
 
-  <h1>This is an about page</h1>
+  <h1>Economy/Climate</h1>
 <!-- Collapse -->
   <div>
-    <b-button v-b-toggle.collapse-1 variant="primary">Toggle Collapse</b-button>
+    <b-button v-b-toggle.collapse-1 variant="primary">Economy</b-button>
     <b-collapse id="collapse-1" class="mt-2">
       <b-card>
-        <p class="card-text">Collapse contents Here</p>
-        <b-button v-b-toggle.collapse-1-inner size="sm">Toggle Inner Collapse</b-button>
+        <p class="card-text">The state's per capita income is $40,828, ranking 39th in the U.S. The state had a median household income of $50,448, making it 22nd in the country and just below the U.S. national mean.[78] Early in its history, Arizona's economy relied on the "five C's": copper (see Copper mining in Arizona), cotton, cattle, citrus, and climate (tourism). Copper is still extensively mined from many expansive open-pit and underground mines, accounting for two-thirds of the nation's output.
+
+Employment
+Total employment 2016
+
+2,379,409
+Total employer establishments 2</p>
+        <b-button v-b-toggle.collapse-1-inner size="sm">Climate</b-button>
         <b-collapse id="collapse-1-inner" class="mt-2">
-          <b-card>Hello!</b-card>
+          <b-card>Arizona is well known for its desert Basin and Range region in the state's southern portions, which is rich in a landscape of xerophyte plants such as the cactus. This region's topography was shaped by prehistoric volcanism, followed by the cooling-off and related subsidence. Its climate has exceptionally hot summers and mild winters. The state is less well known for its pine-covered north-central portion of the high country of the Colorado Plateau (see Arizona Mountains forests).!</b-card>
         </b-collapse>
       </b-card>
     </b-collapse>
