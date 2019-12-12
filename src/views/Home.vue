39764a140e431a98c19feb12316990d4837f69b0
Robi <rahail79@iMac714-462.local>
2019-12-12 14:33:38 -0500

    project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
 
-    <img alt="Vue logo" src="../assets/download.png" />
+    <img alt="Vue logo" style="width: 400px" src="../assets/download.png" />
+ />
     <h1>Arizona</h1>
     <HelloWorld msg="Welcome to Your Vue.js App" />
 
@@ -16,14 +17,15 @@
       <p>The history of Arizona encompasses Spanish, Mexican, and American periods. Arizona was part of the state of Sonora, Mexico from 1822, but the settled population was small.Arizona became a state in 1912 but was primarily rural with an economy based on cattle, cotton, citrus and copper.</p>
       <img alt="Vue logo" style="width: 400px" src="../assets/arizona2.jpg" />
 
-      <b-button v-b-toggle.collapse-1-inner size="sm">Toggle Inner Collapse</b-button>
+      <b-button v-b-toggle.collapse-1-inner size="sm">Location</b-button>
       <b-collapse id="collapse-1-inner" class="mt-2">
-        <b-card>Hello!</b-card>
+        <b-card>Arizona is located in the southwestern quadrant of the conterminous states, bordered by California to the west, Nevada to the northwest, Utah to the north, New Mexico to the east, and the Mexican state of Sonora to the south</b-card>
+        <img alt="Vue logo" style="width: 400px" src="../assets/map.gif" />
+     />
       </b-collapse>
     </b-card>
   </b-collapse>
 </div>
-
   </div>
 </template>
 <script>
